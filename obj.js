82dd6fcfd39dd3ec5b0afea01fedf7dd51b0c998
fuchunki <68526425+fuchunki@users.noreply.github.com>
2023-08-05 16:35:42 +0800

    Update obj.js

--- obj.js
+++ obj.js
@@ -50,4 +50,73 @@ let p5 = {
 	}
 }
 
-let objList=[p1,p2,p3,p4,p5];
+let o1 = {
+	x:10,
+	y:200,
+	draw: function(){
+		playerDraw("red",this.x,this.y,1);
+	}
+}
+
+let o2 = {
+	x:20,
+	y:200,
+	draw: function(){
+		playerDraw("red",this.x,this.y,2);
+	}
+}
+
+let o3 = {
+	x:30,
+	y:200,
+	draw: function(){
+		playerDraw("red",this.x,this.y,3);
+	}
+}
+
+let o4 = {
+	x:40,
+	y:200,
+	draw: function(){
+		playerDraw("red",this.x,this.y,4);
+	}
+}
+
+let o5 = {
+	x:50,
+	y:200,
+	draw: function(){
+		playerDraw("red",this.x,this.y,5);
+	}
+}
+
+let bball = {
+	x:10,
+	y:300,
+	draw: function(){
+	 ctx.fillStyle = "orange";
+   ctx.strokeStyle = "black";
+   ctx.lineWidth = 1;
+	 ctx.beginPath();
+	 ctx.arc (this.x,this.y, unit/2.5, 0, 2*Math.PI);
+	 ctx.fill();
+   ctx.stroke();
+    
+  ctx.beginPath();
+  ctx.moveTo(this.x+unit/2.5, this.y);
+  ctx.lineTo(this.x-unit/2.5, this.y);
+  ctx.moveTo(this.x, this.y+unit/2.5);
+  ctx.lineTo(this.x, this.y-unit/2.5);
+  ctx.stroke();
+
+  ctx.beginPath();
+  ctx.arc (this.x,this.y+unit/2.5, unit/3.5, 3.6, 5.8);
+  ctx.stroke();
+  ctx.beginPath();
+  ctx.arc (this.x,this.y-unit/2.5, unit/3.5, 0.5, 2.7);
+  ctx.stroke();
+    
+	}
+}
+
+let objList=[p1,p2,p3,p4,p5,o1,o2,o3,o4,o5,bball];
