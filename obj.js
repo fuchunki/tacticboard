6f4137b75ff6b154e770067744c3e12fa0355ec7
fuchunki <68526425+fuchunki@users.noreply.github.com>
2023-08-05 15:36:21 +0800

    add playerDraw function

--- obj.js
+++ obj.js
@@ -11,7 +11,7 @@ function playerDraw(colour,x,y,number){
 }
 
 let p1 = {
-	x:100,
+	x:10,
 	y:100,
 	draw: function(){
 		playerDraw("blue",this.x,this.y,1);
@@ -19,68 +19,35 @@ let p1 = {
 }
 
 let p2 = {
-	x:200,
+	x:20,
 	y:100,
 	draw: function(){
-		ctx.fillStyle = "blue";
-		ctx.beginPath();
-		ctx.arc (this.x,this.y, unit/2, 0, 2*Math.PI);
-		ctx.fill();
-    ctx.fillStyle = "white";
-    ctx.textBaseline = 'middle';
-    ctx.textAlign = "center";
-    ctx.font = "30px Arial";
-    ctx.fillText("2", this.x, this.y);
+		playerDraw("blue",this.x,this.y,2);
 	}
 }
 
 let p3 = {
-	x:300,
+	x:30,
 	y:100,
 	draw: function(){
-		ctx.fillStyle = "blue";
-		ctx.beginPath();
-		ctx.arc (this.x,this.y, unit/2, 0, 2*Math.PI);
-		ctx.fill();
-    ctx.fillStyle = "white";
-    ctx.textBaseline = 'middle';
-    ctx.textAlign = "center";
-    ctx.font = "30px Arial";
-    ctx.fillText("3", this.x, this.y);
+		playerDraw("blue",this.x,this.y,3);
 	}
 }
 
 let p4 = {
-	x:400,
+	x:40,
 	y:100,
 	draw: function(){
-		ctx.fillStyle = "blue";
-		ctx.beginPath();
-		ctx.arc (this.x,this.y, unit/2, 0, 2*Math.PI);
-		ctx.fill();
-    ctx.fillStyle = "white";
-    ctx.textBaseline = 'middle';
-    ctx.textAlign = "center";
-    ctx.font = "30px Arial";
-    ctx.fillText("4", this.x, this.y);
+		playerDraw("blue",this.x,this.y,4);
 	}
 }
 
 let p5 = {
-	x:500,
+	x:50,
 	y:100,
 	draw: function(){
-		ctx.fillStyle = "blue";
-		ctx.beginPath();
-		ctx.arc (this.x,this.y, unit/2, 0, 2*Math.PI);
-		ctx.fill();
-    ctx.fillStyle = "white";
-    ctx.textBaseline = 'middle';
-    ctx.textAlign = "center";
-    ctx.font = "30px Arial";
-    ctx.fillText("5", this.x, this.y);
+		playerDraw("blue",this.x,this.y,5);
 	}
 }
-
 
 let objList=[p1,p2,p3,p4,p5];
